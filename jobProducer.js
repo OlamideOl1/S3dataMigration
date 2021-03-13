3098ca9760bcf254d315dee968ed63fabae82184
Olamide <olamideolaoye1@gmail.com>
2021-03-13 10:52:48 +0100

    amend sample queue consumer file - jobProducer.js to handle exceptions

--- jobProducer.js
+++ jobProducer.js
@@ -10,11 +10,21 @@ var Queue = require('bull');
 //   host: '52.86.55.47'
 // })
 
-const videoQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
+// const videoQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
+
+const videoQueue = new Queue('video transcoder', {
+  redis: {
+    port: 6379,
+    host: "52.86.55.47",
+    // maxRetriesPerRequest: null,
+    // enableReadyCheck: false,
+    enableOfflineQueue: false
+  }
+});
 
 // var videoQueue = new Queue('video transcoding', 'redis://52.86.55.47:6379');
 
-const job = videoQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true }).catch(error => alert(error.message));
+const job = videoQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true }).catch(error => console.log(error.message));
 
 
 
