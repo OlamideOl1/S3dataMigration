9b17ff92698d4fc75018e9d2c8ed1bfe082ba5b0
Olamide <olamideolaoye1@gmail.com>
2021-03-13 03:21:17 +0100

    update sample job producer for exception handling

--- jobProducer.js
+++ jobProducer.js
@@ -1,12 +1,31 @@
 debugger;
 
+
 var Queue = require('bull');
 
-const videoQueue = new Queue('video transcodingf', 'redis://52.86.55.47:6379');
+// const Redis = require('ioredis')
+//
+// const redis = new Redis({
+//   port: 6379,
+//   host: '52.86.55.47'
+// })
+
+const videoQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
 
 // var videoQueue = new Queue('video transcoding', 'redis://52.86.55.47:6379');
 
-const job = videoQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true });
+const job = videoQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true }).catch(error => alert(error.message));
+
+
 
 // return;
 videoQueue.close();
+
+// videoQueue.on('error', this.emit.bind(this, 'error'));
+
+// redis.on('error', error => console.log('error'))
+//
+// } catch (err) {
+//         console.log(err);
+//         return null;
+//     }
