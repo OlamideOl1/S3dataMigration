a053db55f85d1e706ae6b84e81e9ac56b5025d0f
Olamide <olamideolaoye1@gmail.com>
2021-03-27 22:43:26 +0100

    set dummy job producer script to generate multiple jobs to test queue

--- jobProducer.js
+++ jobProducer.js
@@ -10,32 +10,42 @@ var Queue = require('bull');
 //   host: '52.86.55.47'
 // })
 
-// const videoQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
+// const objectQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
 
-const videoQueue = new Queue('video transcoder', {
+const objectQueue = new Queue('objectQueue', {
   redis: {
     port: 6379,
-    host: "52.86.55.47",
+    host: "34.229.161.96",
     // maxRetriesPerRequest: null,
     // enableReadyCheck: false,
     enableOfflineQueue: false
   }
 });
 
-// var videoQueue = new Queue('video transcoding', 'redis://52.86.55.47:6379');
+var objectQueue = new Queue('objectQueue', 'redis://52.86.55.47:6379');
 
-const job = videoQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true }).catch(error => console.log(error.message));
+for (var x = 0;x<10;x++){
+
+// objectQueue.add({video: 'http://example.com/video1.mov'}, { removeOnComplete: true }).then(res=>{
+objectQueue.add({video: 'http://example.com/video1.mov'}).then(res=>{
+  console.log("job added");
+}).catch(error => {
+  console.log(error.message);
+});
+
+}
 
 
 
-// return;
-videoQueue.close();
+objectQueue.on('drained', async (job, result) => {
+ console.log("now local drained");
 
-// videoQueue.on('error', this.emit.bind(this, 'error'));
+ // objectQueue.close();
 
-// redis.on('error', error => console.log('error'))
-//
-// } catch (err) {
-//         console.log(err);
-//         return null;
-//     }
+})
+
+objectQueue.on('global:drained', function(jobId, progress) {
+  console.log("now global drained")
+
+  // objectQueue.close();
+});
