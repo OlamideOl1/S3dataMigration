a5524095e848a1569a746ba5bf567cd1ab53a4a6
Olamide <olamideolaoye1@gmail.com>
2021-03-27 22:47:43 +0100

    set dummy job consumer script to process multiple test jobs in queue

--- jobWorker.js
+++ jobWorker.js
@@ -1,62 +1,28 @@
-debugger;
-
-
-
 var Queue = require('bull');
 
-var videoQueue = new Queue('video transcoder', 'redis://52.86.55.47:6379');
+const objectQueue = new Queue('objectQueue', {
+  redis: {
+    port: 6379,
+    host: "34.229.161.96",
+    enableOfflineQueue: false
+  }
+});
 
-videoQueue.process(function(job, done) {
-
-  // job.data contains the custom data passed when the job was created
-  // job.id contains id of this job.
-
-  // transcode video asynchronously and report progress
-  // job.progress(42);
-
-
+objectQueue.process(function(job, done) {
 
   console.log(job.data);
 
   done();
 
   console.log("now done");
-  // videoQueue.close().then(jobDone);
-
-  // return Promise.resolve({ framerate: 29.5 /* etc... */ });
-
-  // call done when finished
-  // done(null, { framerate: 29.5 /* etc... */ });
-
-  // done();
-
-
-  // or give a error if error
-  // done(new Error('error transcoding'));
-  //
-  // // or pass it a result
-  // done(null, { framerate: 29.5 /* etc... */ });
-  //
-  // // If the job throws an unhandled exception it is also handled correctly
-  // throw new Error('some unexpected error');
-
-
-  //   var after100 = _.after(1, function() {
-  //   videoQueue.close().then(function() {
-  //     console.log('done');
-  //   });
-  // });
-
-  // videoQueue.on('drained', videoQueue.close().then(function() {console.log('done');}));
-videoQueue.getJobCounts().then(res => console.log('second Job Count:\n',res["waiting"]+res["active"]));
-
-videoQueue.count().then(res => console.log('new count variable is :\n',res));
-
-
-
-
-  videoQueue.on("waiting", console.log("now waiting"));
-
-videoQueue.getJobCounts().then(res => console.log('third Job Count:\n',res));
 
 }).catch(error => alert(error.message));
+
+objectQueue.on('completed', async (job, result) => {
+  console.log("now completed")
+
+})
+
+objectQueue.on('drained', function(jobId, progress) {
+  objectQueue.close();
+});
