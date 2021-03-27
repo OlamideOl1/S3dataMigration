2a02faa498089e73be11da81374cb6e36d2fac8e
Olamide <olamideolaoye1@gmail.com>
2021-03-27 15:26:16 +0100

    set object list to be generated from production database, send completion update once all rows are now updated at database.

--- s3JobProducer.js
+++ s3JobProducer.js
@@ -2,16 +2,21 @@ const mariadb = require('mariadb');
 var Queue = require('bull');
 var timediff = require('timediff');
 
-var connectionLimit = 10
+
+const connectionLimit = 10;
+const databaseHost = "34.229.161.96";
+const dbUser = "root";
+const dbPassword = 'Ab@123456';
+const databaseName = "userImageData";
 
 var dbConfig = {
-  host: "34.229.161.96",
-  user: "root",
-  password: 'Ab@123456',
-  database: "userImageData",
+  host: databaseHost,
+  user: dbUser,
+  password: dbPassword,
+  database: databaseName,
   connectionLimit: connectionLimit,
-  waitForConnections: true, // Default value.
-  queueLimit: 0 // Unlimited - default value.
+  // waitForConnections: true, // Default value.
+  // queueLimit: 0 // Unlimited - default value.
 };
 
 const redisHost = "34.229.161.96";
@@ -20,20 +25,13 @@ const redisPort = 6379;
 var redisParam = {
   port: redisPort,
   host: redisHost,
-  // maxRetriesPerRequest: null,
-  // enableReadyCheck: false,
   enableOfflineQueue: false
 }
 
 var redisParamOffline = {
   port: redisPort,
   host: redisHost,
-  // maxRetriesPerRequest: null,
-  // enableReadyCheck: false,
-  // enableOfflineQueue: false
 }
-
-
 
 const objectQueue = new Queue('objectQueue', {
   redis: redisParam
@@ -43,71 +41,65 @@ const executionCompleteQueue = new Queue('executionCompleteQueue', {
   redis: redisParamOffline
 });
 
-executionCompleteQueue.count().then(res => {
-
-  if (res) {
-
-    console.log('executionCompleteQueue count is not empty :\n');
-
-    console.log('Producer node has already completed successfully, set RERUN_UPLOAD in variable file to true to start process again:\n');
-
-    executionCompleteQueue.close();
-
-    process.exit();
-  }
-
-  executionCompleteQueue.close();
-
-}).catch(err => {
-
-  console.log('Redis server is not running, review it is available on: ' + redisHost + " : " + redisPort);
-
-  executionCompleteQueue.close();
-
-  process.exit();
-
-});
-
+var legacyS3ObjectPrefix = "image";
 var targetObjectPrefix = "avatar/";
-
-//var pool = mariadb.createPool(dbConfig);
-
 var stageTableforMerge = "stageTableforMerge";
-
+var databaseTabletoUpdate = "ImageData";
+var tableColumnNametoUpdate = "Imagepath";
 var startDateTime = new Date();
-
 var targetS3Bucket = "newproductionbucket77";
-
+var sqlRowLimit = 5000000;
 var maxUploadPerInvocation = 1000000;
-
 var setRecordsPerBatch = 30000;
-
 var s3bulkList = [];
-
 var allBatchPromiseList = [];
 
-for (var i = 0; i < 25; i++) {
+initiateObjectSelection();
 
-  var oldImagePath = "image/avatar" + i + ".txt";
+function initiateObjectSelection() {
 
-  s3bulkList.push(oldImagePath);
+  selectFromDB()
+    .then(res => {
+      if (res.length > 0) {
+        loadS3ObjectstoQueue(res, 0, res.length);
+      } else {
+        console.log("No records selected for migration");
+        //send migration completed indicatotr to queue
+        objectQueue.add({
+            "migrationcompleted"
+          }).then(res => console.log("add to queue successful"))
+          .catch(error => {
+            console.log("add to queue failed see error = " + error.message);
+          });
+
+        objectQueue.close();
+        pool.end();
+
+      }
+    })
+    .catch(err => console.log("error occured =>" + err.message));
 
 }
 
-loadS3toQueueDatabase(s3bulkList, 0, s3bulkList.length);
+async function selectFromDB() {
+  var s3bulkList = [];
+  try {
+    const rows = await pool.query("select " + tableColumnNametoUpdate + " from " + databaseTabletoUpdate + " where " + tableColumnNametoUpdate + " like " + legacyS3ObjectPrefix + "% limit " + sqlRowLimit)
+    s3bulkList = rows.map(element => element[tableColumnNametoUpdate]);
+    return s3bulkList;
+  } catch (err) {
+    throw err;
+    return s3bulkList;
+  }
+}
 
-function loadS3toQueueDatabase(s3bulkList, startIndex, endIndex) {
+function loadS3ObjectstoQueue(s3bulkList, startIndex, endIndex) {
 
   var loopEndIndex = ((endIndex - startIndex) < maxUploadPerInvocation) ? endIndex : (maxUploadPerInvocation + startIndex);
-
   var bulkList = [];
-
   var recordsPerBatch = setRecordsPerBatch;
-
   var eachList = [];
-
   var batchCounter = 1;
-
   var allBatchPromiseList = [];
 
   for (var i = startIndex; i < loopEndIndex; i++) {
@@ -120,73 +112,47 @@ function loadS3toQueueDatabase(s3bulkList, startIndex, endIndex) {
     }
 
     var oldImagePath = s3bulkList[i];
-
     var sourceObjectSplit = oldImagePath.split('/');
-
     var newImagePath = targetObjectPrefix + sourceObjectSplit[sourceObjectSplit.length - 1];
 
     s3ObejectforList.oldImagePath = oldImagePath;
     s3ObejectforList.newImagePath = newImagePath;
 
-    // s3ObejectforList.oldImagePath = "'" + oldImagePath + "'";
-    // s3ObejectforList.newImagePath = "'" + newImagePath + "'";
-
     bulkList.push(s3ObejectforList);
 
     if (batchCounter == recordsPerBatch || batchCounter == loopEndIndex) {
-
       allBatchPromiseList.push(objectQueue.add({
           bucketObjects: bulkList
         }).then(res => console.log("add to queue successful"))
         .catch(error => {
           console.log("add to queue failed see error = " + error.message);
-          // Will now remove last item (failed item) from db batch so it will not get loaded to database
-
-          //bulkList.pop(eachList);
-
         }));
-
-      //recordsPerBatch = recordsPerBatch + setRecordsPerBatch;
-
-	  recordsPerBatch += (( loopEndIndex - i - 1) < setRecordsPerBatch) ? (loopEndIndex - i - 1) : setRecordsPerBatch;
-
+      recordsPerBatch += ((loopEndIndex - i - 1) < setRecordsPerBatch) ? (loopEndIndex - i - 1) : setRecordsPerBatch;
       bulkList = [];
-
     }
-
     batchCounter++;
-
   }
 
   Promise.allSettled(allBatchPromiseList).then(function() {
 
     if (loopEndIndex == endIndex) {
-
-      console.log('ALL QUEUE UPLOAD ARE NOW COMPLETED');
-
+      console.log('Current batch of Queue upload now completed, Awaiting processing by agents to complete');
       var endDateTime = new Date();
-
       var expended = timediff(startDateTime, endDateTime, 'YDHmS');
-
       console.log("process expended: " + expended.hours + ":" + expended.minutes + ":" + expended.seconds);
 
-      //    pool.end();
-
-      objectQueue.close();
-
     } else {
-
       console.log("Current set is done inserting, now commencing next set of batch upload");
-
-      //  loadS3toQueueDatabase(s3bulkList, loopEndIndex, s3bulkList.length);
-
       setTimeout(function() {
         console.log("now waiting");
         loadS3toQueueDatabase(s3bulkList, loopEndIndex, s3bulkList.length);
       }, 150);
-
     }
-
   });
-
 }
+
+objectQueue.on('global:drained', function(jobId, progress) {
+  //Previous batch now complete, check for pending
+  initiateObjectSelection();
+  // videoQueue.close();
+});
