3a3059b69776fa513329c4fbaf8e44de1958a50e
Olamide <olamideolaoye1@gmail.com>
2021-03-30 08:24:16 +0100

    modify createsampleDBTable to use create table, insert sample records and use pool batch query

--- createSampleDBTable.js
+++ createSampleDBTable.js
@@ -1,11 +1,11 @@
-var numberOfObjectsToUpload = 200;
+var numberOfObjectsToUpload = 100;
 
 var bucketName = "legacybucket77";
 
-const mariadb = require('mariadb/callback');
+const mariadb = require('mariadb');
 
-const pool = mariadb.createPool({
-  host: "3.233.221.242",
+var pool = mariadb.createPool({
+  host: "54.152.26.74",
   user: "root",
   password: 'Ab@123456',
   database: "userImageData",
@@ -14,8 +14,6 @@ const pool = mariadb.createPool({
 var tableName = "ImageData";
 var tableColumnName = "Imagepath";
 
-//set content for dummy object to be uploaded.
-var objectBody = "this is a test content for objects to be uploaded";
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region
@@ -28,45 +26,28 @@ s3 = new AWS.S3({
   region: 'us-east-1'
 });
 
-// call S3 to retrieve upload file to specified bucket
-var uploadParams = {
-  Bucket: bucketName,
-  Key: '',
-  Body: objectBody,
-  ACL: "private"
-};
 // var file = process.argv[3];
-
-var response = await pool.query("CREATE TABLE IF NOT EXISTS " + tableName + " (ID int NOT NULL AUTO_INCREMENT , " + tableColumnName + " VARCHAR(255) , PRIMARY KEY (ID))");
 
 var path = require('path');
 
+var pushList = [];
+
 for (var i = 0; i < numberOfObjectsToUpload; i++) {
 
-  uploadParams.Key = "image/avatar" + i + ".txt";
-
-  insertTableData(uploadParams.Key);
+  pushList.push("image/avatar" + i + ".txt");
 
 }
 
-async function insertTableData(oldImagePath) {
+var objectUpdateQuery = "INSERT INTO " + tableName + "(" + tableColumnName + ") VALUES (?)";
 
-  try {
+pool.query("CREATE TABLE IF NOT EXISTS " + tableName + " (ID int NOT NULL AUTO_INCREMENT , " + tableColumnName + " VARCHAR(255) , PRIMARY KEY (ID))")
+  .then(res => {
+    return pool.batch(objectUpdateQuery, pushList);
+  })
+  .then(res => {
+    console.log("completed");
+    pool.end();
+  })
+  .catch(err => console.log("error occured " + err.message))
 
-    // const rows = await conn.query("SELECT 1 as val");
-    // // rows: [ {val: 1}, meta: ... ]
-
-    var objectUpdateQuery = "INSERT INTO " + tableName + "(" + tableColumnName + ") VALUES ('" + oldImagePath + "')";
-
-    var res = await pool.query(objectUpdateQuery).catch(function() {
-      console.log("Promise Rejected");
-    });;
-    // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
-    if (res.affectedRows) {
-      dbUpdateFlag = true;
-    }
-
-  } catch (err) {
-    throw err;
-  }
-}
+// insertTableData(pushList);
