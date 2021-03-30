b8bde15446daaa3259ef6c5b4566532a3ee13ef4
Olamide <olamideolaoye1@gmail.com>
2021-03-30 21:10:56 +0100

    add variable file for docker compose deeployment option

---
+++ dockerComposeIamAccess/variables.tf
@@ -0,0 +1,47 @@
+variable "SOURCE_S3_BUCKET" {
+}
+
+variable "TARGET_S3_BUCKET" {
+}
+
+variable "AWS_REGION" {
+}
+
+variable "TARGET_OBJECT_PREFIX" {
+}
+
+variable "TEMP_TABLE_FOR_UPDATE" {
+}
+
+variable "DATABASE_HOST" {
+}
+
+variable "DB_USER" {
+}
+
+variable "DB_PASSWORD" {
+}
+
+variable "DATABASE_NAME" {
+}
+
+variable "LEGACY_S3_OBJECT_PREFIX" {
+}
+
+variable "DATABASE_TABLE_TO_UPDATE" {
+}
+
+variable "TABLE_COLUMN_NAME_TO_UPDATE" {
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+}
+
+variable "ECR_S3_JOB_PRODUCER_REPOSITORY_NAME" {
+}
+
+variable "ECR_S3_JOB_CONSUMER_REPOSITORY_NAME" {
+}
